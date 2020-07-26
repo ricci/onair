ae220f15813d7f3fd558af073547b4a42f0e433d
Robert Ricci <ricci@cs.utah.edu>
2020-07-26 14:25:05 +0100

    Improvements for MQTT
    Send messages that should work more nicely with home-assistant, and
    start stubbing out a thread that will handle commands from MQTT

--- onair.py
+++ onair.py
@@ -6,9 +6,19 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import json 
 import paho.mqtt.client as mqtt
 import threading
+import atexit
+
+MQTT_ROOT = "onair"
+MQTT_STATE = "{}/state".format(MQTT_ROOT)
+MQTT_COMMAND = "{}/state/set".format(MQTT_ROOT)
+MQTT_AVAILABLE = "{}/state/available".format(MQTT_ROOT)
+
+MQTT_ON = "ON"
+MQTT_OFF = "OFF"
 
 led = LED(17)
 led.off()
+
 
 class MyHandlerForHTTP(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -16,42 +26,55 @@ class MyHandlerForHTTP(BaseHTTPRequestHandler):
             led.on()
             self.send_response(200)
             self.end_headers()
-            mqttc.publish("onair/event", payload="on")
+            mqttc.publish(MQTT_STATE, payload=MQTT_ON)
         elif self.path == "/off":
             led.off()
             self.send_response(200)
             self.end_headers()
-            mqttc.publish("onair/event", payload="off")
+            mqttc.publish(MQTT_STATE, payload=MQTT_OFF)
         elif self.path == "/toggle":
             led.toggle()
             self.send_response(200)
             self.end_headers()
+            mqttc.publish(MQTT_STATE, payload=(MQTT_ON if led.is_lit else MQTT_OFF))
         elif self.path == "/status":
             self.send_response(200)
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
             json_str = json.dumps({ "led" : led.is_lit })
             self.wfile.write(json_str.encode(encoding='utf_8'))
-        #self.send_response(200)
-        #self.send_header('Content-Type', 'text/plain')
-        #self.wfile.write(bytes('Hello World\n', 'UTF-8'))
-        #self.wfile.write(bytes('You have requested '+self.path+'\n', 'UTF-8'))
 
-class myThread (threading.Thread):
+class HTTPThread (threading.Thread):
    def run(self):
-      print ("Starting thread")
+      print ("Starting http thread")
       httpd.serve_forever()
-      print ("Exiting thread")
+      print ("Exiting http thread")
+
+class MQTTThread (threading.Thread):
+   def run(self):
+      print ("Starting mqtt thread")
+      #httpd.serve_forever()
+      print ("Exiting mqtt thread")
+
 
 server_address = ('', 8000)
 
 mqttc = mqtt.Client()
 mqttc.connect("10.0.0.66")
-mqttc.publish("onair/daemon", payload="start")
+mqttc.publish(MQTT_AVAILABLE, payload=MQTT_ON)
+
+@atexit.register
+def mqttSignoff():
+    mqttc.publish(MQTT_AVAILABLE, payload=MQTT_OFF)
 
 print("About to create server")
 httpd = HTTPServer(server_address, MyHandlerForHTTP)
 print("About to serve forever")
-httpThread = myThread()
-httpThread.run()
+httpThread = HTTPThread()
+httpThread.start()
+
+mqttThread = MQTTThread()
+mqttThread.start()
+
+mqttThread.join()
 httpThread.join()
