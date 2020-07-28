c0f2ac20aa604686619a44bd74d9d84c7964fed5
Robert Ricci <ricci@cs.utah.edu>
2020-07-28 13:07:19 -0600

    Simple client to monitor whether I'm on air or not
    Uses the simple heuristic that if anything has the mic open, I'm on
    air

---
+++ onair-client.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import pulsectl
+import paho.mqtt.client as mqtt
+
+MQTT_ROOT = "onair"
+MQTT_STATE = "{}/state".format(MQTT_ROOT)
+MQTT_COMMAND = "{}/state/set".format(MQTT_ROOT)
+MQTT_AVAILABLE = "{}/state/available".format(MQTT_ROOT)
+
+MQTT_ON = "ON"
+MQTT_OFF = "OFF"
+
+pulse = pulsectl.Pulse('onair-client')
+mqttc = mqtt.Client()
+mqttc.connect("10.0.0.66")
+
+def mic_on():
+    global pulse
+    for e in pulse.source_output_list():
+        # Not sure what this is, but it seems to always be there, so it doesn't
+        # count
+        if e.name != "parec":
+            return True
+    return False
+
+currentState = None
+
+# This lets us treat the event listening loop as a simple blocking call - we aren't
+# allowed to do the listing calls from the callback
+def handle_pa_event(ev):
+    raise pulsectl.PulseLoopStop
+
+pulse.event_mask_set('source_output')
+pulse.event_callback_set(handle_pa_event)
+while True:
+    listening = mic_on()
+    if currentState is None or currentState != listening:
+        if listening:
+            print("Now listening")
+            mqttc.publish(MQTT_COMMAND, payload=MQTT_ON)
+        else:
+            print("No longer listening")
+            mqttc.publish(MQTT_COMMAND, payload=MQTT_OFF)
+    currentState = listening
+    pulse.event_listen()
